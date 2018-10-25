4ab30649f16416a5caf6ed4cde8c09e7a2f2f43c
unknown <wkogucki@gmail.com>
2018-10-25 13:35:39 +0200

    Creating input field and ul

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import Map from './Map.js'
 
 class List extends Component {
 
+  state = {
+    query: ''
+  }
+
+  updateQuery = (event) => {
+    this.setState({query: event.target.value})
+  }
 
   render() {
     return (
-      <div id='list'>Asasa</div>
+      <div id='list'>My Favourite Places
+        <form className='search-form'>
+          <input className='search-location' placeholder='Find location' onChange={this.updateQuery }/>
+        </form>
+        <ul className='search-list'>
+        
+        </ul>
+      </div>
     )
   }
 }
