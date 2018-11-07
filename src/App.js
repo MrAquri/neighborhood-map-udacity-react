38ec8aa89abec6797a4715fafb7d1746d4f848d5
unknown <wkogucki@gmail.com>
2018-11-07 12:57:08 +0100

    Creating stateless function instead of a component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Map from './components/Map.js'
 
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App" role='application'>
-          <div className='header' role='banner'>Neighborhood Map</div>
-          <div className='container' role='main'>
-            <Map />
-          </div>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App" role='application'>
+        <div className='header' role='banner'>Neighborhood Map</div>
+        <div className='container' role='main'>
+          <Map />
+        </div>
+    </div>
+  )
 }
 
 export default App;
